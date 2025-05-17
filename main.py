b47538889460d8b6bb8a4daa76d398573561ee32
Ignemia <andy.opaleckych@gmail.com>
2025-05-17 10:06:24 +0200

    add audio models

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 def main():
-    print("Hello, World!")
+    load_image_pipeline()
+    load_embeddings_pipeline()
+    load_text_pipeline()
+    load_audio_pipeline()  
+    
+    
     
 if __name__ == "__main__":
     main()
